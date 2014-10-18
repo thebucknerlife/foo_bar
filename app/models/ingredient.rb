64b81585188b26b22899082a20e22fa00bfb2466
Greg Buckner <gbuckner23@gmail.com>
2014-10-18 14:48:28 -0700

    created ingredeints controller, added .quantity instance method to ingredients to grab their quantity, added some menu stuff to the layout file, added links everywhere

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -3,4 +3,8 @@ class Ingredient < ActiveRecord::Base
   has_many :ingredient_recipes
   has_many :recipes, through: :ingredient_recipes
 
+  def quantity(recipe)
+    return self.ingredient_recipes.find_by(recipe: recipe).quantity
+  end
+
 end
